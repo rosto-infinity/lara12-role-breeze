388c9e21228bf13e11ba8a099b5123c706f3115b
ROSTAND LELE <rostoinfocus@gmail.com>
2025-03-06 21:59:45 +0100

    delete

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -1,10 +1,10 @@
 <?php
-
+ 
 namespace App\Http\Controllers;
-
-use App\Models\Product;
+ 
 use Illuminate\Http\Request;
-
+use App\Models\Product;
+ 
 class ProductController extends Controller
 {
     public function index()
